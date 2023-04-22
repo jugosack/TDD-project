8d6ab71268c909eb40fb68654924494a0528e032
jugosack <jugoslav_ackoski@yahoo.com>
2023-04-22 13:16:39 +0300

    should raise exception for negative integer - method called factorial that takes one argument, an integer N - Passing test

--- solver.rb
+++ solver.rb
@@ -2,6 +2,7 @@ class Solver
     def reverse (str)
         str.reverse
     end
+
     def fizzbuzz(num)
         if num % 3 == 0 && num % 5 == 0
             'fizzbuzz'
@@ -12,6 +13,11 @@ class Solver
         else 
             num.to_s
         end
+    end
 
+    def factorial(sequence)
+        if sequence < 0
+            raise StandardError,'Please enter 0 or positive number'
+        end
     end
 end
